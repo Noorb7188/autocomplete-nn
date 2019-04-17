7a6f789bd5e2939143a51708eb97bb42f9ced6e4
NaremanPro <violette1988@gmail.com>
2019-04-17 10:40:52 +0300

    added eventListener function

---
+++ public/dom.js
@@ -0,0 +1,7 @@
+var inputValue = document.getElementById('form')[0];
+inputValue.addEventListener('keyup', function(e) {
+  e.preventDefault();
+  fetchData(inputValue.value, );
+})
+
+function fetchData ()
